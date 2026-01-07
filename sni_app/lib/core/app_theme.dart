f67cb37bef5eabd8f1067f59cf9f82b9f9683414
Kele_Kele006 <kelepony006@gmail.com>
2026-01-07 21:25:29 +0100

    chore: corretto tema
    corretto il tema dell'applicazione

--- sni_app/lib/core/app_theme.dart
+++ sni_app/lib/core/app_theme.dart
@@ -2,13 +2,29 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static ThemeData darkTheme = ThemeData(
-    brightness: Brightness.dark,
     useMaterial3: true,
-    colorSchemeSeed: Colors.redAccent,
-    scaffoldBackgroundColor: Colors.black,
-    textTheme: const TextTheme(
-      headlineMedium: TextStyle(fontWeight: FontWeight.bold),
-      bodyMedium: TextStyle(fontSize: 16),
+
+    colorScheme: const ColorScheme.dark(
+      primary: Color.fromARGB(255, 0, 0, 0),     // nero
+      secondary: Color.fromARGB(255, 0, 0, 0),   // nero
+      surface: Color.fromARGB(255, 51, 51, 51),   // contorni partner
+    ),
+    scaffoldBackgroundColor: const Color.fromARGB(255, 0, 0, 0),
+
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color.fromARGB(255, 0, 0, 0),
+      foregroundColor: Color.fromARGB(255, 255, 255, 255), //sempre bianco menu hamburger e freccia indietro
+      centerTitle: true,
+    ),
+
+    drawerTheme: const DrawerThemeData(
+      backgroundColor: Color(0xFF121212),
+    ),
+
+    listTileTheme: const ListTileThemeData(
+      iconColor: Colors.white,
+      textColor: Colors.white,
     ),
   );
 }
+ 
